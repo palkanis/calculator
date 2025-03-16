9fe698d3cfdc0647707864b26ac824a25cc27160
palkanis <palkanis@msu.edu>
2025-03-16 14:56:51 +0530

    Add more functionality

--- script.js
+++ script.js
@@ -16,13 +16,12 @@ function divNum(a, b) {
     return a / b;
 };
 
-let inputOne;
-let inputTwo;
-let operation;
 
 function operate(operator, num1, num2) {
 
     parseInt(operator);
+    parseInt(num1);
+    parseInt(num2);
 
     if (operator === '+') {
         return addNum(num1, num2);
@@ -38,19 +37,63 @@ function operate(operator, num1, num2) {
     }
 }
 
-const display = document.querySelector('#display')
-const buttons = document.getElementsByClassName('btn')
-const digitArray = Array.from(buttons)
+const display = document.querySelector('#display');
+const buttons = document.getElementsByClassName('btn');
+const ops = document.getElementsByClassName('op');
+const operatorArray = Array.from(ops);
+const digitArray = Array.from(buttons);
+const clrBtn = document.querySelector('#x');
+const eqBtn = document.querySelector('#eq');
 
-function displayNum (arr) {
-    for (let i = 0; i < arr.length; i++) {
-        document.getElementById(`${arr[i].id}`).addEventListener('click', () => {
-            display.textContent = arr[i].textContent;
-            inputOne = display.textContent;
+let inputOne = '';
+let inputTwo = '';
+let operation = '';
+let enterSecNum = false;
+
+for (let i = 0; i < digitArray.length; i++) {
+    const idItem = digitArray[i];
+    document.getElementById(idItem.id).addEventListener('click', function() {
+        if (!enterSecNum) {
+            display.textContent += idItem.textContent;
+            inputOne = Number(display.textContent);
             console.log(inputOne)
-        }) 
-    } 
-    
+        }
+        else {
+            display.textContent += idItem.textContent;
+            inputTwo = Number(display.textContent)
+            console.log(inputTwo)
+        }
+
+        
+    })
 }
 
-displayNum(digitArray)
+for (let i = 0; i < operatorArray.length; i++) {
+    const opItem = operatorArray[i];
+    document.getElementById(opItem.id).addEventListener('click', function() {
+        operation = opItem.textContent;
+        display.textContent = ''
+        enterSecNum = true;
+        console.log(operation)
+    })
+}
+
+eqBtn.addEventListener('click', () => {
+    display.textContent = operate(operation, Number(inputOne), Number(inputTwo))
+    console.log(display.textContent)
+})
+
+clrBtn.addEventListener("click",()=>{
+    display.textContent='';
+    inputOne = '';
+    inputTwo = '';
+    operation = '';
+    enterSecNum = false;
+})
+
+//displayNum(digitArray)
+//console.log(display.textContent)
+
+//console.table(digitArray)
+  
+  
